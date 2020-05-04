becb174f394f72035e75081590528381c8578804
ruiwei33 <r.wei33@gmail.com>
2020-05-04 10:47:19 -0700

    Update CalculateEstimates.m
    Uncertainties updated for A0, dA, ninf(na), p(x1), A0 & ninf, A0 and p, and ninf & p

--- src/CalculateEstimates.m
+++ src/CalculateEstimates.m
@@ -72,25 +72,56 @@ Obs.sigdAv=sqrt(diag(Obs.CdA));
 Obs.sigdA=reshape(Obs.sigdAv,D.nt,D.nR)';
 %4.2) Uncertainty (variance) of the Manning terms
 % Estimate.QhatUnc.n=(Estimate.stdnPost./Estimate.nhat).^2;
+Estimate.QhatUnc.na=(Estimate.stdnaPost./Prior.meanna).^2;
+Estimate.QhatUnc.x1=(log(Obs.w./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)) .* ...
+                    (Estimate.stdx1Post*ones(1,D.nt))).^2;
+
 Estimate.QhatUnc.w=(2/3*Obs.sigw./Obs.w).^2;
-Estimate.QhatUnc.A0=(5/3.*Estimate.stdA0Post*ones(1,D.nt)./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2;
-Estimate.QhatUnc.dA=(5/3.*Obs.sigdA./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2;
+% Estimate.QhatUnc.A0=(5/3.*Estimate.stdA0Post*ones(1,D.nt)./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2; % change
+% Estimate.QhatUnc.dA=(5/3.*Obs.sigdA./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2;
 Estimate.QhatUnc.S=(1/2.*Obs.sigS./Obs.S).^2;
 
+Estimate.QhatUnc.A0=((5/3-Prior.meanx1*ones(1,D.nt)) .* ...
+                    (Estimate.stdA0Post*ones(1,D.nt)) ./ ...
+                    (Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2;
+Estimate.QhatUnc.dA=((5/3-Prior.meanx1*ones(1,D.nt)) .* ...
+                    Obs.sigdA ./ ...
+                    (Estimate.A0hat*ones(1,D.nt)+Obs.dA)).^2;
+     
 % Need to revise this for variable n... 
 
-%4.3) Estimate correlation coefficient between A0 & n
-% for i=1:D.nR,
-%     R=corrcoef([C.thetaA0(i,:); C.thetan(i,:);]');
-%     Estimate.rho_A0n(i,1)=R(1,2);
-% end
+%4.3) Estimate correlation coefficient between A0 & na, A0 & x1, na & x1
+ for i=1:D.nR,
+     R_A0na=corrcoef([C.thetaA0(i,:); C.thetana(i,:);]');
+     Estimate.rho_A0na(i,1)=R_A0na(1,2);
+     R_A0x1=corrcoef([C.thetaA0(i,:); C.thetax1(i,:);]');
+     Estimate.rho_A0x1(i,1)=R_A0x1(1,2);
+     R_nax1=corrcoef([C.thetana(i,:); C.thetax1(i,:);]');
+     Estimate.rho_nax1(i,1)=R_nax1(1,2);
+ end
 
 %4.4) Estimate uncertainty of Manning's Q
-%4.4.1) Estimate uncertainty in Q due to cross-correlation of A0 & n
-% Estimate.QhatUnc.A0n=(-2*(5/3).*Estimate.rho_A0n*ones(1,D.nt) .* ...
-%     (Estimate.stdA0Post*ones(1,D.nt)) .* (Estimate.stdnPost*ones(1,D.nt)) ./ ...
-%     (Estimate.nhat*ones(1,D.nt)) ./ (Estimate.A0hat*ones(1,D.nt)+Obs.dA)  );
-% 
+%4.4.1) Estimate uncertainty in Q due to cross-correlation of A0 & na, na & x1, x1 & A0 
+Estimate.QhatUnc.A0na=-2*(Estimate.rho_A0na*ones(1,D.nt)) .* ...
+                      (5/3-Prior.meanx1*ones(1,D.nt)) .* ...
+                      (Estimate.stdnaPost*ones(1,D.nt)) .* ...
+                      (Estimate.stdA0Post*ones(1,D.nt)) ./ ...
+                      (Prior.meanna*ones(1,D.nt)) ./ ...
+                      (Estimate.A0hat*ones(1,D.nt)+Obs.dA);
+              
+Estimate.QhatUnc.nax1=-2*(Estimate.rho_nax1*ones(1,D.nt)) .* ...
+                      (Estimate.stdx1Post*ones(1,D.nt)) .* ...
+                      (Estimate.stdnaPost*ones(1,D.nt)) .* ...
+                      log(Obs.w./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)) ./ ...
+                      (Prior.meanna*ones(1,D.nt));
+                  
+Estimate.QhatUnc.A0x1=2*(Estimate.rho_A0x1*ones(1,D.nt)) .* ...
+                      (Estimate.stdA0Post*ones(1,D.nt)) .* ...
+                      (Estimate.stdx1Post*ones(1,D.nt)) .* ...
+                      (5/3-Prior.meanx1*ones(1,D.nt)) .* ...
+                      log(Obs.w./(Estimate.A0hat*ones(1,D.nt)+Obs.dA)) ./ ...
+                      (Estimate.A0hat*ones(1,D.nt)+Obs.dA);
+
 % %4.4.2) Estimate total Q uncertinty
 % Estimate.QhatUnc.Hat=sqrt( Estimate.QhatUnc.n+mean(Estimate.QhatUnc.w,2)+...
 %     mean(Estimate.QhatUnc.A0,2)+mean(Estimate.QhatUnc.dA,2)+mean(Estimate.QhatUnc.S,2)+...
@@ -110,6 +141,27 @@ Estimate.QhatUnc.S=(1/2.*Obs.sigS./Obs.S).^2;
 % 
 % %5 all estimates
 % 
+
+%4.4.2) Estimate total Q uncertinty
+Estimate.QhatUnc.Hat=sqrt( Estimate.QhatUnc.na+mean(Estimate.QhatUnc.x1)+mean(Estimate.QhatUnc.w,2)+...
+    mean(Estimate.QhatUnc.A0,2)+mean(Estimate.QhatUnc.dA,2)+mean(Estimate.QhatUnc.S,2)+...
+    mean(Estimate.QhatUnc.A0na,2)+mean(Estimate.QhatUnc.nax1,2)+mean(Estimate.QhatUnc.A0x1,2));
+
+%4.4.2) Estimate total Q uncertinty wrt time
+Estimate.QhatUnc.HatAll=sqrt( Estimate.QhatUnc.na*ones(1,D.nt)+Estimate.QhatUnc.x1+Estimate.QhatUnc.w+...
+    Estimate.QhatUnc.A0+Estimate.QhatUnc.dA+Estimate.QhatUnc.S+...
+    Estimate.QhatUnc.A0na+Estimate.QhatUnc.nax1+Estimate.QhatUnc.A0x1);
+
+%4.4.3) Discharge error budget
+Estimate.QerrVarSum(1:D.nR,1)=Estimate.QhatUnc.na;
+Estimate.QerrVarSum(1:D.nR,1)=mean(Estimate.QhatUnc.x1,2);
+Estimate.QerrVarSum(1:D.nR,2)=mean(Estimate.QhatUnc.A0,2);
+Estimate.QerrVarSum(1:D.nR,3)=mean(Estimate.QhatUnc.dA,2);
+Estimate.QerrVarSum(1:D.nR,4)=mean(Estimate.QhatUnc.w,2);
+Estimate.QerrVarSum(1:D.nR,5)=mean(Estimate.QhatUnc.S,2);
+
+%5 all estimates
+
 Estimate.AllQ = 1./Estimate.nhatAll .* ...
         ( (Estimate.A0hat-AllObs.A0Shift)*ones(1,DAll.nt)+AllObs.dA).^(5/3).*AllObs.w.^(-2/3).*sqrt(AllObs.S) ;
 
